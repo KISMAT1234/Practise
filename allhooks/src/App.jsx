7a7f6c750fda35967bf7f268add394a31893ddbb
KISMAT1234 <kismatbohora123@gmail.com>
2024-01-28 20:29:30 +0545

    resize

--- allhooks/src/App.jsx
+++ allhooks/src/App.jsx
@@ -1,43 +1,27 @@
 
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
-function App(){
-  const[form, setForm] = useState({
-    name:"",
-    email: "",
-    password:"",
-    address:"",
-  });
 
-const handleInput = (e) => {
-  const name = e.target.name;
-  const email = e.target.email;
-  const password = e.target.password;
-  const address= e.target.address;
+ const App = () => {
 
-  setForm((prev)=>{
-    return {...prev, [name]:value}
-  });
-
-  console.log(name);
-
-}
+  const [windows, setwindow]= useState(window.screen.width);
   
+  const currentScreenWidth=()=>{
+    setwindow(() => window.innerWidth);
+
+  }
   
+  useEffect(()=>{
+    window.addEventListener('resize',currentScreenWidth)
+    return () => {
+      window.removeEventListener("resize",currentScreenWidth);
+    }
+  });
   return (
     <> 
-    <h1>Registration form</h1>
-    <form submit="">
-       <input type="text" name="name" onChange={handleInput} value={form.name} placeholder="name"/><br></br>
-       
-       <input type="email" name="email" onChange={handleInput} value={form.email} placeholder="emial"/><br></br>
-       
-       <input type="password" name="password" onChange={handleInput} value={form.password} placeholder="password"/><br></br>
-       
-       <input type="text" name="address" onChange={handleInput} value={form.address} placeholder="address"/><br></br>
-       <button >register</button>
-    </form>
-    <p>{`My name is ${form.name}  and I live at ${form.address}`}</p>
+        <wrapper>
+           <h1> The size of the window is <span> {windows} </span>  </h1>
+        </wrapper>
     </>
 
   )
