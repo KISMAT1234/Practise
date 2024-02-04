616a106be06a50dca8cb606e79ca9ef8f836fddd
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-04 19:20:37 +0545

    tailwind

---
+++ todolist/src/todolist/UserDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {DeleteAllUser} from "./DeleteAllUser";
+
+const UserDetails = () => {
+    return(
+         <>
+
+           <DeleteAllUser/>
+
+
+
+        </>
+    )
+}
